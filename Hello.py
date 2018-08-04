c3da141fe4ce505ab8bd421d42f6c229f722c412
adamkarren <adam.t.karren@gmail.com>
2018-08-04 16:12:05 -0500

    For Loops

--- Hello.py
+++ Hello.py
@@ -1,2 +1,8 @@
 print("Hello world")
 print("I'm going to build a chatbot one day!")
+
+#For Loops
+
+texasCities = ["Houston","Austin","Dallas","San Antonio"]
+for texas in texasCities:
+    print(texas)
