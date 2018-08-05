aace8612fd4706b40e5c687022505e853017913a
adamkarren <adam.t.karren@gmail.com>
2018-08-05 15:22:57 -0500

    List of Lists

--- Hello.py
+++ Hello.py
@@ -6,3 +6,23 @@ print("I'm going to build a chatbot one day!")
 texasCities = ["Houston","Austin","Dallas","San Antonio"]
 for texas in texasCities:
     print(texas)
+
+
+#List of Lists
+texasRanking = ["Houston,1","Austin,2","Dallas,3","San Antonio,4"]
+finalRanking = []
+
+for rows in texasRanking:
+    interimRanking = rows.split(',')
+    finalRanking.append(interimRanking)
+print(finalRanking)
+
+
+#Unique Mechanisms in List of Lists
+firstCityPair = finalRanking[0]
+firstCity = firstCityPair[0]
+print(firstCityPair)
+print(firstCity)
+
+alternativeCity = finalRanking [1][0]
+print(alternativeCity)
